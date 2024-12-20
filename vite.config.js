49f2bff92caef459a531c460a49d39be87d0158f
Ahmed Amer <amer73090@gmail.com>
2024-12-20 17:17:18 +0200

    test index

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    historyApiFallback: true, 
+  },
+  build: {
+    outDir: "dist", 
+  },
+  publicDir: 'public',
+});
